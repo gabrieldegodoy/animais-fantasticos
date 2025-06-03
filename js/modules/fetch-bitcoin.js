97cc440a29be1502498a4a7d3f8b526e514eec8d
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-06-03 14:25:50 -0300

    Refatora function fetchBitcoin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,8 +1,8 @@
-export default function initFetchBitcoin() {
-  fetch("https://blockchain.info/ticker")
+export default function fetchBitcoin(url, target) {
+  fetch(url)
     .then((r) => r.json())
     .then((b) => {
-      const btcPreco = document.querySelector(".btc-preco");
+      const btcPreco = document.querySelector(target);
       btcPreco.innerText = (1000 / b.BRL.sell).toFixed(4);
     })
     .catch((erro) => {
